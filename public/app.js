301f8a3a6df3d427279398bed4d392fc6559eecf
Eric Baltutis <34050626+baltu008@users.noreply.github.com>
2018-07-21 02:11:17 -0500

    add broken stuff

--- public/app.js
+++ public/app.js
@@ -1,53 +1,55 @@
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$(document).ready(function () {
+
+  // Grab the articles as a json
+  $.getJSON("/articles", function (data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].articles.title + "<br />" + data[i].articles.url + "</p>");
     }
   });
-  
-  
+
+
   // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
+  $(document).on("click", "p", function () {
     // Empty the notes from the note section
     $("#comments").empty();
     // Save the id from the p tag
     var thisId = $(this).attr("data-id");
-  
+
     // Now make an ajax call for the Article
     $.ajax({
       method: "GET",
       url: "/articles/" + thisId
-    }).then(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#comments").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#comments").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new comment body
-        $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new comment, with the id of the article saved to it
-        $("#comments").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a comment in the article
-        if (data.comment) {
-          // Place the title of the comment in the title input
-          $("#titleinput").val(data.comment.title);
-          // Place the body of the comment in the body textarea
-          $("#bodyinput").val(data.comment.body);
-        }
-      });
+    }).then(function (data) {
+      console.log(data);
+      // The title of the article
+      $("#comments").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#comments").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new comment body
+      $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new comment, with the id of the article saved to it
+      $("#comments").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a comment in the article
+      if (data.comment) {
+        // Place the title of the comment in the title input
+        $("#titleinput").val(data.comment.title);
+        // Place the body of the comment in the body textarea
+        $("#bodyinput").val(data.comment.body);
+      }
+    });
   });
-  
+
   // When you click the savecomment button
-  $(document).on("click", "#savecomment", function() {
+  $(document).on("click", "#savecomment", function () {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
-  
+
     // Run a POST request to change the comment, using what's entered in the inputs
     $.ajax({
       method: "POST",
@@ -60,15 +62,15 @@ $.getJSON("/articles", function(data) {
       }
     })
       // With that done
-      .then(function(data) {
+      .then(function (data) {
         // Log the response
         console.log(data);
         // Empty the comments section
         $("#comments").empty();
       });
-  
+
     // Also, remove the values entered in the input and textarea for comment entry
     $("#titleinput").val("");
     $("#bodyinput").val("");
   });
-  
+});
--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
+var request = require("request");
 var mongoose = require("mongoose");
-var semantic = require("./semantic.json");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
@@ -28,8 +23,6 @@ app.set("view engine", "handlebars");
 //* MIDDLEWARE
 // =============================================================================
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
@@ -44,64 +37,70 @@ mongoose.connect(MONGODB_URI);
 //* ROUTES
 // =============================================================================
 
-app.get("/", function(req, res) {
+app.get("/scrape", function (req, res) {
+  url = "https://newsapi.org/v2/everything?sources=cnn&apiKey=40545117cb354db0bfbd00f6b5b80451"
 
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-    //  console.log(dbArticle);
-    // If we were able to successfully find Articles, send them back to the client
-    // res.json(dbArticle);
-     var articleData = {
-       data: dbArticle
-     }
-   //  console.log('Article Data ' + articleData.data);
+  request(url, function (err, response, html) {
+    
+    var $ = cheerio.load(response);
+    
+    if (!err) {
+      // console.log(html);
+    }
 
-     res.render('index', articleData);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// A GET route for scraping the informit website
-app.get("/scrape", function(req, res) {
-  //* First, we grab the body of the html with request
-  axios.get("https://www.informit.com/articles/index.aspx?st=60206").then(function(response) {
-
-    //* Then, we load that into cheerio and save it to $ (a shorthand selector)
-    var $ = cheerio.load(response.data);
-
-    $("dl").each((i, element) => {
+    let json = JSON.parse(html).articles
+    // console.log(json)
+  
+    let collection = JSON.parse(html).articles.filter(article => article.title)
+    console.log(collection);
+    collection.forEach((element) => {
       // var link = $(element).children().attr("href");
-      var title = $(element).children("dt").children("a").text();
-      var link = $(element).children("dt").children("a").attr('href');
-      var summary = $(element).children('.intro').text();
-      console.log(summary);
-    // console.log(title);
-     // console.log(link);
+      // var title = $(element).children("dt").children("a").text();
+      // var link = $(element).children("dt").children("a").attr('href');
+      // var summary = $(element).children('.intro').text();
+      // console.log(summary);
 
       // Create a new Article using the `result` object built from scraping
-      db.Article.create({
-        title: title,
-        link: link,
-        summary: summary
+      if(element) {
+        console.log(element);
+        db.Article.create({
+        title: element.title
+        // link: articles.url,
+        // summary: articles.description
       })
-        .then(function(dbArticle) {
-          // View the added result in the console
-        //  console.log(dbArticle);
+        .then(function (dbArticle) {
         })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
+        .catch(function (err) {
           return res.json(err);
         });
+      }
     });
 
     // If we were able to successfully scrape and save an Article, send a message to the client
-  // res.send("Scrape Complete"); // << Success
+    // res.send("Scrape Complete"); // << Success
     res.redirect("/");
-  });
+  })
+
+
+});
+
+
+app.get("/", function (req, res) {
+
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function (dbArticle) {
+
+      var articleData = {
+        data: dbArticle
+      }
+
+      res.render('index', articleData);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
 // =============================================================================
@@ -109,58 +108,58 @@ app.get("/scrape", function(req, res) {
 // =============================================================================
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
 
   // Grab every document in the Articles collection
   db.Article.find({})
-    .then(function(dbArticle) {
-      
+    .then(function (dbArticle) {
+
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's comment
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
     // ..and populate all of the comments associated with it
     .populate("comment")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Comment
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // Create a new comment and pass the req.body to the entry
   db.Comment.create(req.body)
-    .then(function(dbComment) {
+    .then(function (dbComment) {
       // If a Comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Comment
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
